79e4f5b40fcee4c45f023dd0e5f3d53a20b3e54e
Navaron Bracke <brackenavaron@gmail.com>
2023-10-12 23:40:34 +0200

    add stub for channel tests

---
+++ test/mobile_scanner_method_channel_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile_scanner/mobile_scanner_method_channel.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  MethodChannelMobileScanner platform = MethodChannelMobileScanner();
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      platform.methodChannel,
+      (MethodCall methodCall) async {
+        // TODO: fix the mock method call handler
+        return '42';
+      },
+    );
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      platform.methodChannel,
+      null,
+    );
+  });
+
+  // TODO: write tests using `test()` and `expect()` that verify the behavior of the channels
+}
