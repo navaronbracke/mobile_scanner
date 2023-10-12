decf5894719fb1b567548b13ce0a56c43674e072
Navaron Bracke <brackenavaron@gmail.com>
2023-10-13 00:04:34 +0200

    remove ration from exports

--- lib/mobile_scanner.dart
+++ lib/mobile_scanner.dart
@@ -2,7 +2,6 @@ export 'src/enums/camera_facing.dart';
 export 'src/enums/detection_speed.dart';
 export 'src/enums/mobile_scanner_error_code.dart';
 export 'src/enums/mobile_scanner_state.dart';
-export 'src/enums/ratio.dart';
 export 'src/enums/torch_state.dart';
 export 'src/mobile_scanner.dart';
 export 'src/mobile_scanner_controller.dart';
