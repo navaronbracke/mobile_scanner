0f5de67445b61dd1a0e139f9fc945a25dca50d2d
Navaron Bracke <brackenavaron@gmail.com>
2023-10-19 14:38:58 +0200

    add raw value constructor to authorization state

--- lib/src/enums/mobile_scanner_authorization_state.dart
+++ lib/src/enums/mobile_scanner_authorization_state.dart
@@ -7,5 +7,18 @@ enum MobileScannerAuthorizationState {
   authorized,
 
   /// The user denied the required permissions.
-  denied
+  denied;
+
+  factory MobileScannerAuthorizationState.fromRawValue(int value) {
+    switch (value) {
+      case 0:
+        return MobileScannerAuthorizationState.undetermined;
+      case 1:
+        return MobileScannerAuthorizationState.authorized;
+      case 2:
+        return MobileScannerAuthorizationState.denied;
+      default:
+        throw ArgumentError.value(value, 'value', 'Invalid raw value.');
+    }
+  }
 }
