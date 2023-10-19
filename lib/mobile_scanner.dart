558836a6fc086ddd2c911f5f6f8b78dfcc667cff
Navaron Bracke <brackenavaron@gmail.com>
2023-10-19 13:47:16 +0200

    fix imports in barrel file

--- lib/mobile_scanner.dart
+++ lib/mobile_scanner.dart
@@ -1,18 +1,28 @@
+export 'src/enums/address_type.dart';
+export 'src/enums/barcode_format.dart';
+export 'src/enums/barcode_type.dart';
 export 'src/enums/camera_facing.dart';
 export 'src/enums/detection_speed.dart';
+export 'src/enums/email_type.dart';
+export 'src/enums/encryption_type.dart';
+export 'src/enums/mobile_scanner_authorization_state.dart';
 export 'src/enums/mobile_scanner_error_code.dart';
-export 'src/enums/mobile_scanner_state.dart';
+export 'src/enums/phone_type.dart';
 export 'src/enums/torch_state.dart';
-export 'src/mobile_scanner.dart';
 export 'src/mobile_scanner_controller.dart';
 export 'src/mobile_scanner_exception.dart';
+export 'src/mobile_scanner_platform_interface.dart';
+export 'src/objects/address.dart';
 export 'src/objects/barcode.dart';
 export 'src/objects/barcode_capture.dart';
-export 'src/objects/mobile_scanner_arguments.dart';
-
-import 'mobile_scanner_platform_interface.dart';
-
-class MobileScanner {
-  // TODO: delegate to platform interface instance
-  // MobileScannerPlatform.instance.foo();
-}
+export 'src/objects/calendar_event.dart';
+export 'src/objects/contact_info.dart';
+export 'src/objects/driver_license.dart';
+export 'src/objects/email.dart';
+export 'src/objects/geo_point.dart';
+export 'src/objects/mobile_scanner_state.dart';
+export 'src/objects/person_name.dart';
+export 'src/objects/phone_number.dart';
+export 'src/objects/sms.dart';
+export 'src/objects/url_bookmark.dart';
+export 'src/objects/wifi.dart';
