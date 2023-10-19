5c72171a8b5d4bf35fced5230d98797ec2299021
Navaron Bracke <brackenavaron@gmail.com>
2023-10-19 12:40:07 +0200

    implement new mobiel scanner state class

---
+++ lib/src/objects/mobile_scanner_state.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+
+import 'package:mobile_scanner/src/enums/camera_facing.dart';
+import 'package:mobile_scanner/src/enums/torch_state.dart';
+import 'package:mobile_scanner/src/mobile_scanner_exception.dart';
+
+/// The state of a [MobileScannerController].
+class MobileScannerState {
+  /// Create a new [MobileScannerState] instance.
+  const MobileScannerState({
+    required this.cameraDirection,
+    required this.size,
+    required this.torchState,
+    required this.zoomScale,
+    this.error,
+  });
+
+  /// The facing direction of the camera.
+  final CameraFacing cameraDirection;
+
+  /// The error that occurred while setting up or using the canera.
+  final MobileScannerException? error;
+
+  /// The size of the camera output.
+  final Size size;
+
+  /// The current state of the flashlight of the camera.
+  final TorchState torchState;
+
+  /// The current zoom scale of the camera.
+  final double zoomScale;
+
+  /// Create a copy of this state with the given parameters.
+  MobileScannerState copyWith({
+    CameraFacing? cameraDirection,
+    MobileScannerException? error,
+    Size? size,
+    TorchState? torchState,
+    double? zoomScale,
+  }) {
+    return MobileScannerState(
+      cameraDirection: cameraDirection ?? this.cameraDirection,
+      error: error,
+      size: size ?? this.size,
+      torchState: torchState ?? this.torchState,
+      zoomScale: zoomScale ?? this.zoomScale,
+    );
+  }
+}
