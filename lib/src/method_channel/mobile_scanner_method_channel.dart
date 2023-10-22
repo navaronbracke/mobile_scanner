8df743c9281a8b7ffcb350de94847ced13251aca
Navaron Bracke <brackenavaron@gmail.com>
2023-10-22 15:08:00 +0200

    temp native impl

--- lib/src/method_channel/mobile_scanner_method_channel.dart
+++ lib/src/method_channel/mobile_scanner_method_channel.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:mobile_scanner/src/enums/mobile_scanner_authorization_state.dart';
+import 'package:mobile_scanner/src/enums/mobile_scanner_error_code.dart';
+import 'package:mobile_scanner/src/enums/torch_state.dart';
+import 'package:mobile_scanner/src/mobile_scanner_exception.dart';
 import 'package:mobile_scanner/src/mobile_scanner_platform_interface.dart';
+import 'package:mobile_scanner/src/objects/mobile_scanner_options.dart';
 
 /// An implementation of [MobileScannerPlatform] that uses method channels.
 class MethodChannelMobileScanner extends MobileScannerPlatform {
@@ -11,6 +19,108 @@ class MethodChannelMobileScanner extends MobileScannerPlatform {
   /// The event channel that sends back scanned barcode events.
   @visibleForTesting
   final eventChannel = const EventChannel('dev.steenbakker.mobile_scanner/scanner/event');
+
+  StreamSubscription<Object?>? _eventsSubscription;
+
+  @override
+  Future<bool> analyzeImage(String path) {
+    /*
+        events ??= _eventChannel
+        .receiveBroadcastStream()
+        .listen((data) => _handleEvent(data as Map));
+
+    return _methodChannel
+        .invokeMethod<bool>('analyzeImage', path)
+        .then<bool>((bool? value) => value ?? false);
+    */
+  }
+
+  @override
+  Future<Map<String, Object?>> initialize(MobileScannerOptions options) async {
+    _eventsSubscription ??= eventChannel.receiveBroadcastStream().cast<Map<String, Object?>>().listen(_handleEvent);
+
+    Map<String, Object?>? configuration;
+
+    final Map<String, Object?> arguments = {
+      'facing': cameraDirection.index,
+      'returnImage': returnImage,
+      'speed': detectionSpeed.index,
+      'timeout': detectionTimeoutMs,
+      'torch': torchEnabled,
+    };
+
+    // TODO: refactor Android to use the raw value as well
+    if (formats != null) {
+      if (Platform.isIOS || Platform.isMacOS) {
+        arguments['formats'] = formats.map((e) => e.rawValue).toList();
+      } else if (Platform.isAndroid) {
+        arguments['formats'] = formats.map((e) => e.index).toList();
+      }
+    }
+
+    try {
+      configuration = await methodChannel.invokeMapMethod<String, Object?>(
+        'start',
+        arguments,
+      );
+    } on PlatformException catch (error) {
+      throw MobileScannerException(
+        errorCode: MobileScannerErrorCode.genericError,
+        errorDetails: MobileScannerErrorDetails(
+          code: error.code,
+          details: error.details as Object?,
+          message: error.message,
+        ),
+      );
+    }
+
+    // TODO
+  }
+
+  @override
+  Future<void> setTorchState(TorchState state) async {
+    if (state != TorchState.unavailable) {
+      await methodChannel.invokeMethod('torch', state.index);
+    }
+  }
+
+  @override
+  Future<void> setZoomScale(double zoomScale) async {
+    await methodChannel.invokeMethod('setScale', zoomScale);
+  }
+
+  @override
+  Future<bool> requestCameraPermission() async {
+    final MobileScannerAuthorizationState state =
+        MobileScannerAuthorizationState.fromRawValue(await methodChannel.invokeMethod<int>('state') ?? -1);
+
+    switch (state) {
+      case MobileScannerAuthorizationState.undetermined:
+        try {
+          final bool result = await methodChannel.invokeMethod<bool>('request') ?? false;
+
+          return result;
+        } on PlatformException catch (error) {
+          throw MobileScannerException(
+            errorCode: MobileScannerErrorCode.genericError,
+            errorDetails: MobileScannerErrorDetails(
+              code: error.code,
+              details: error.details as Object?,
+              message: error.message,
+            ),
+          );
+        }
+      case MobileScannerAuthorizationState.denied:
+        return false;
+      case MobileScannerAuthorizationState.authorized:
+        return true;
+    }
+  }
+
+  @override
+  Future<void> resetZoomScale() {
+    return methodChannel.invokeMethod<void>('resetScale');
+  }
 
   @override
   Future<void> updateScanWindow(Rect? window) async {
@@ -25,6 +135,9 @@ class MethodChannelMobileScanner extends MobileScannerPlatform {
 
   @override
   Future<void> dispose() async {
+    _eventsSubscription?.cancel();
+    _eventsSubscription = null;
+
     await methodChannel.invokeMethod('stop');
   }
 }
